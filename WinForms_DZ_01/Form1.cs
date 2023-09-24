1e678f2024d04d6a695e23d07f25ccdb4d51fdb6
AseevArtjom <Artjom4ik2008@gmail.com>
2023-09-24 11:28:30 +0300

    DZ_01 was done

--- WinForms_DZ_01/Form1.cs
+++ WinForms_DZ_01/Form1.cs
@@ -6,5 +6,21 @@ namespace WinForms_DZ_01
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string str1 = "Асеев Артём";
+            string str2 = "15 лет,г.Мариуполь";
+            string str3 = "Обучающийся группы ПВ221";
+
+            int count_of_symbols = str1.Length + str2.Length + str3.Length;
+            double average_value = count_of_symbols / 3;
+
+            MessageBox.Show(str1);
+            MessageBox.Show(str2);
+            MessageBox.Show(str3);
+            MessageBox.Show(count_of_symbols + " символов");
+            MessageBox.Show("Среднее значение символов : " + average_value);
+        }
     }
 }
